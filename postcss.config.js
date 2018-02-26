5f150367a3bdc33703189ac1f7cdd6c7cb8b372d
rhiokim <rhio.kim@gmail.com>
2018-02-27 01:24:01 +0900

    add postcss-assets example

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,12 @@ module.exports = {
   plugins: [
       require('precss'),
       require('autoprefixer'),
-      require('postcss-import')
+      require('postcss-import'),
+      require('postcss-assets')({
+        loadPaths: ['images', 'fonts'],
+        basePath: 'static/',
+        baseUrl: '/static',
+        cachebuster: true
+      })
   ]
 }
